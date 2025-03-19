c675e429840c7040e25aeb9242a92477a5b95b89
Jakub FLeischer <jakub.fleischer03@gmail.com>
2025-03-19 14:14:30 +0100

    slowly implementing everything

--- Code/RaTGen.py
+++ Code/RaTGen.py
@@ -3,24 +3,29 @@ import pandas as pd
 
 class RaTGen:
     def __init__(self):
-        self.traj = None        #maybe Pandas?
+        self.traj = pd.DataFrame()        #maybe Pandas?
         self.max_q = None       #Contains every joint's max angle
         self.max_accel = None   #Contains max acceleration
         self.max_vel = None     #Contains max velocity
-        self.dt = None          #Contains time diff
+        self.dt = .1          #Contains time diff
+
 
     def generate_sin(self, amp, freq, phase=0, t0=0, tmax=2*np.pi):    #Generates a sin trajectory
+        self.traj['sin_1'] = amp * np.sin(2 * np.pi * freq * np.arange(t0, tmax, self.dt) + phase)
 
     def generate_custom(self, fun, t0, tmax):
+        t = np.linspace(t0, tmax, int((tmax-t0)/self.dt))
+        self.traj['custom'] = fun(t)
+
 
     def generate_punch(self, force):     #Whats required?
-
+        return -69  # not implemented
     def generate_movement(self, ):    #Hard coded movement as list of Transforms
-
-    def add_traj(self, traj1, traj2):   #Add two trajectories
-
+        return -69  # not implemented
+    def add_traj(self, traj1, traj2, t_diff=0):   #Add two trajectories
+        return -69  # not implemented
     def add_random_noise(self, noise):  #Adds random noise to trajectory    maybe custom Noise function?
-
+        return -69  # not implemented
 
 
     def set_max_q(self, max_q):     #Sets max_q
@@ -47,5 +52,6 @@ class RaTGen:
     def get_traj(self):     #Returns trajectory
         return self.traj
     def write_csv(self, outfile):      #Writes to file-> q as csv
-
+        return -69  # not implemented
     def read_csv(self, infile):        #Reads from file
+        return -69  # not implemented
--- Code/test.py
+++ Code/test.py
@@ -1,1 +1,6 @@
-print("ALLOOO")
+from RaTGen import *
+
+rt = RaTGen()
+
+rt.generate_sin(1, 1, 0)
+print(rt.get_traj())
