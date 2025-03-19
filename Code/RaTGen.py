40fcb92d7451ece97c3ecbe7068e7667c6c1376b
Jakub FLeischer <jakub.fleischer03@gmail.com>
2025-03-19 13:50:20 +0100

    added maxvel(accel

--- Code/RaTGen.py
+++ Code/RaTGen.py
@@ -1,13 +1,16 @@
 class RaTGen:
-
     def __init__(self):
         self.traj = None        #maybe Pandas?
+        self.max_q = None       #Contains every joint's max angle
+        self.max_accel = None   #Contains max acceleration
+        self.max_vel = None     #Contains max velocity
 
-    def generate_sin(self):
 
-    def generate_custom(self, fun):
+    def generate_sin(self, amp, freq, phase=0):
 
-    def generate_punch(self):
+    def generate_custom(self, fun, t0, tmax, dt):
+
+    def generate_punch(self, ):
 
     def generate_movement(self):
 
