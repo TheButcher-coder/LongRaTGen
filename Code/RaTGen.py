537075a8564fb2514c5348f41e0545573b45d20f
Jakub FLeischer <jakub.fleischer03@gmail.com>
2025-03-19 13:47:59 +0100

    added RaTGen.py

---
+++ Code/RaTGen.py
@@ -0,0 +1,18 @@
+class RaTGen:
+
+    def __init__(self):
+        self.traj = None        #maybe Pandas?
+
+    def generate_sin(self):
+
+    def generate_custom(self, fun):
+
+    def generate_punch(self):
+
+    def generate_movement(self):
+
+    def add_traj(self, traj1, traj2):
+
+    def write_traj(self, outfile):
+
+    def read_traj(self, infile):
