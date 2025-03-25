f05be6c2e3757c1ac0fd71604069af4c14058b83
Jakub FLeischer <jakub.fleischer03@gmail.com>
2025-03-25 16:26:19 +0100

    added stuff here and there. rethought concept. Bees and flowers. Bees and flowers...

--- Code/RaTGen.py
+++ Code/RaTGen.py
@@ -10,27 +10,42 @@ class RaTGen:
         self.dt = .1          #Contains time diff
 
 
+    def make_traj(self, x, y, z, alpha, beta, gamma):
+        #construct 3d Trajectory with 6DOF and append to traj
+        if len(x) == len(y) == len(z) == len(alpha) == len(beta) == len(gamma):
+            temp = np.zeros(4)
+            temp[3, :3] = np.array([x, y, z])
+
+            self.traj.append(np.array([x, y, z, alpha, beta, gamma]))
+
     def generate_sin(self, amp, freq, phase=0, t0=0, tmax=2*np.pi):    #Generates a sin trajectory
-        self.traj.append(amp * np.sin(2 * np.pi * freq * np.arange(t0, tmax, self.dt) + phase))
+        return amp * np.sin(2 * np.pi * freq * np.arange(t0, tmax, self.dt) + phase)
+
+    def generate_cos(self, amp, freq, phase=0, t0=0, tmax=2*np.pi):    #Generates a cos trajectory
+        return amp * np.cos(2 * np.pi * freq * np.arange(t0, tmax, self.dt) + phase)
 
     def generate_custom(self, fun, t0, tmax):
-        t = np.linspace(t0, tmax, int((tmax-t0)/self.dt))
-        self.traj.append(fun(t))
+        t = np.arange(t0, tmax, self.dt)
+        #self.traj.append(fun(t))
+        return fun(t)
 
-    def generate_punch(self, ):     #Whats required?
+    def generate_punch(self, t0, tmax, a, b, c):     #Whats required?
         #https: // www.desmos.com / calculator / z8txe5wywh?lang = de
-        return -69  # not implemented
-    def generate_movement(self, input):    #Hard coded movement as list of Transforms
-        self.traj.append(input)
-    def add_traj(self, traj1, traj2, t_diff=0):   #Add two trajectories
-        len1 = len(traj1)
-        len2 = len(traj2)
-        if len1 > len2:
-            traj2 = np.append(traj2, np.zeros(len1-len2))
-        else:
-            traj1 = np.append(traj1, np.zeros(len2-len1))
+        t = np.linspace(t0, tmax, int((tmax-t0)/self.dt))
+        return ( a*t^(2*b)*np.exp(-c*t))  # not implemented
 
-        self.traj.append(traj1 + traj2)
+
+    #def generate_movement(self, input):    #Hard coded movement as list of Transforms
+     #   self.traj.append(input)
+    #def add_traj(self, traj1, traj2, t_diff=0):   #Add two trajectories
+    #    len1 = len(traj1)
+    #    len2 = len(traj2)
+    #    if len1 > len2:
+    #        traj2 = np.append(traj2, np.zeros(len1-len2))
+    #    else:
+    #        traj1 = np.append(traj1, np.zeros(len2-len1))
+#
+     #   self.traj.append(traj1 + traj2)
 
     def add_traj(self, num1, num2):
         len1 = len(self.traj[num1])
@@ -42,8 +57,10 @@ class RaTGen:
 
         self.traj[num1] += self.traj[num2]
 
-    def add_random_noise(self, n):  #returns noise vector to add to trajectory
-        return -69  # not implemented
+    def add_random_noise(self, amp, t0, tmax):
+        t = np.arange(t0, tmax, self.dt)
+        noise = amp * np.random.randn(len(t))
+        return noise
 
     def del_traj(self, num):    #Deletes trajectory
         del self.traj[num]
--- Code/test.py
+++ Code/test.py
@@ -1,6 +1,22 @@
 from RaTGen import *
+from mpl_toolkits import mplot3d
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
 
 rt = RaTGen()
+rt.set_dt(.01)
 
-rt.generate_sin(1, 1, 0)
-print(rt.get_traj())
+fun = lambda x: x**(2)/4
+
+x = rt.generate_sin(1, 1)
+y = rt.generate_sin(1,  1)
+z = rt.generate_custom(fun, 0, 2*np.pi)
+
+ax.plot3D(x, y, z)
+
+plt.show()
