c6a90b022f1c74f1a815b9f20d8c6d67a1821f90
Jakub FLeischer <jakub.fleischer03@gmail.com>
2025-06-13 10:06:27 +0200

    first communication protocol test

--- tcp_test/client.py
+++ tcp_test/client.py
@@ -1,9 +1,15 @@
 import socket
 import pickle
 import numpy as np
+import pandas as pd
+from time import sleep
+
 
 HOST = 'localhost'  # IP-Adresse des Servers
 PORT = 6969        # Port des Servers
+
+data = pd.read_csv('random_100x6.csv', header=None).to_numpy()
+
 
 # Socket erstellen
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -12,10 +18,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         print(f"Verbunden mit {HOST}:{PORT}")
 
-        points = [(0, 0, 0, 0, 0, 0), (np.pi/2, 0, 0, 0, 0, 0), (np.pi, 0, 0, 0, 0, 0)]
+        for p in data:
+            # Warte bis bot ready schickt
+            ready = 0
+            while ready == 0:
+                sleep(0.1)
+                ready = s.recv(1024)
+                print(f"Empfangen: {ready.decode('utf-8')}")
 
-        s.sendall(pickle.dumps(points))
-        print(f"Gesendet: {points}")
+            s.sendall(pickle.dumps(p))
+            print(f"Gesendet: {p}")
 
     except Exception as e:
         print(f"Fehler: {e}")
--- tcp_test/host.py
+++ tcp_test/host.py
@@ -1,5 +1,6 @@
 import socket
 import pickle
+from time import sleep
 
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_socket.bind(('localhost', 6969))
@@ -8,24 +9,43 @@ tcp_socket.listen(1)
 while True:
     print("Waiting for connection")
     connection, client = tcp_socket.accept()
+    print("Connected to client:", client)
+    done = 0
+    while done == 0:
+        try:
 
-    try:
-        print("Connected to client:", client)
 
-        # Read all data until the client closes the connection
-        data = b""
-        while True:
-            packet = connection.recv(4096)
-            if not packet:
-                break
-            data += packet
+            # Send ready
+            response = "1"
+            connection.sendall(response.encode('utf-8'))
+            print("Sent ready=1")
 
-        # Unpickle full message
-        obj = pickle.loads(data)
-        print("Received:", obj)
+            #read all data
+            #data = b""
+            data = connection.recv(8*4096)
 
-    except Exception as e:
-        print("Error:", e)
+            # Debug: Print raw data
+            #print("Raw data received:", data)
 
-    finally:
-        connection.close()
+             # Unpickle full message
+            try:
+                obj = pickle.loads(data)
+                print("Received:", obj)
+            except Exception as e:
+                print("Error during unpickling:", e)
+
+            response = "0"
+            print("Sent ready=0")
+            connection.sendall(response.encode('utf-8'))
+            # Fake wait time
+            print("Waiting for 5 seconds before sending response")
+            sleep(5)
+            # Send a response back to the client
+            response = "1"
+            print("sent ready=1")
+            connection.sendall(response.encode('utf-8'))
+
+        except Exception as e:
+            print("Error:", e)
+
+    connection.close()
