7cededc2323f8642ddec9bcfebd29a42803d5d90
Jakub FLeischer <jakub.fleischer03@gmail.com>
2025-08-01 09:21:05 +0200

    Client update

--- tcp_test/client.py
+++ tcp_test/client.py
@@ -1,14 +1,20 @@
 import socket
 import pickle
+import struct
 import numpy as np
+from numpy import pi
 import pandas as pd
 from time import sleep
 
 #oajnwdjabndo
-HOST = 'localhost'  # IP-Adresse des Servers
+HOST = '192.168.0.254'  # IP-Adresse des Servers
 PORT = 6969        # Port des Servers
 
-data = pd.read_csv('random_100x6.csv', header=None).to_numpy()
+data = pd.read_csv('data.csv', delimiter=',')
+#print(data)
+#pi = 3.14
+#data = [[0, 0, 0, 0, 0, 0], [180, 0, 0, 0, 0, 0]]#, [pi/2, 0, 0, 0, 0, 0], [pi, 0, 0, 0, 0, 0]]
+#print(struct.pack([0, 0, 0, 0, 0, 0]))
 
 
 # Socket erstellen
@@ -17,19 +23,29 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         # Verbindung zum Server aufbauen
         s.connect((HOST, PORT))
         print(f"Verbunden mit {HOST}:{PORT}")
-
-        for p in data:
+        #begüßen
+        print("Greeting with 42")
+        s.sendall(struct.pack('f', 42))
+        for i in range(len(data)):
+            p = data.iloc[i]
+            print(p)
             # Warte bis bot ready schickt
             ready = 0
             while True:
-                ready = s.recv(1024).decode('utf-8')
+                #ready = s.recv(1024).decode('utf-8')
+                ready = list(s.recv(1024))[0]
                 print(f"Received: {ready}")
-                if ready == "1":
+                if ready == 1:
                     break
                 sleep(0.1)
-
-            s.sendall(pickle.dumps(p))
-            print(f"Gesendet: {p}")
+            #wait 5s before sending data for more robustness
+            #print("DEBUG: Waiting 5s before starting to send Data")
+            sleep(.1)
+            #Send data to bot
+            #for c in p:
+            print(f"DEBUG: Sending Data: {p} \n as\n {struct.pack('ffffff', p)}")
+            s.sendall(struct.pack('ffffff', p))
+            #print(f"Gesendet: {p}")
 
     except Exception as e:
         print(f"Fehler: {e}")
